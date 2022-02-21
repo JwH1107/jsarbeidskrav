38d103cf05b45844ba6f88939c1fc65c6a37da3e
JwH1107 <hanjon97@gmail.com>
2022-02-21 13:56:22 +0100

    fourth commit

--- script/script.js
+++ script/script.js
@@ -1,1 +1,5 @@
 //alert("Velkommen til din shoppingliste!")
+
+const groceries = document.getElementsByClassName("groceries");
+const userInput = document.getElementById("userInput");
+const item = document.getElementById("item");
