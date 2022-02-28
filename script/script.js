8eb407138d094a4656c44aebd767921b39269bd2
JwH1107 <hanjon97@gmail.com>
2022-02-28 14:05:35 +0100

    fifth commit

--- script/script.js
+++ script/script.js
@@ -1,13 +1,27 @@
 //alert("Velkommen til din shoppingliste!")
+//alert(
+//  "Her kan du legge til varer du trenger, har nok av, pris på varer og fjerne varer du har handlet ferdig eller ikke trenger mer av"
+//);
 
-const groceries = document.getElementsByClassName("groceries");
-const userInput = document.getElementById("userInput");
-const addItem = document.getElementById("addItem");
+var groceries = document.getElementById("groceries");
+var items = document.getElementById("items");
 
-addItem.addEventListener("click", function () {
-  var paragraph = document.createElement("p");
-  paragraph.classList.add("input");
-  paragraph.innerText = userInput.value;
-  addItem.appendChild(paragraph);
-  userInput.value = "";
-});
+itemArray = [];
+
+function addItem() {
+  let userInput = document.getElementById("user-input").value;
+
+  itemArray.push({
+    name: userInput,
+  });
+
+  console.log("nytt array", itemArray);
+  listItems();
+}
+
+function listItems() {
+  for (let i = 0; i < itemArray.length; i++)
+    items.innerHTML = `<li>
+        <h3>${itemArray[i].name}</h3>
+      </li>`;
+}
